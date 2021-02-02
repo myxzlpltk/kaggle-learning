0dca6ef7f8a6247933b400e55e678e72a28f69c7
Saddam Azy <myxzlpltk@gmail.com>
2021-02-02 15:03:53 +0700

    your first machine learning model

--- Intro to Machine Learning/main.py
+++ Intro to Machine Learning/main.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
 import pandas as pd
 
 # Path of the file to read
@@ -6,8 +8,25 @@ iowa_file_path = 'data/home-data-for-ml-course/train.csv'
 # Read data
 home_data = pd.read_csv(iowa_file_path)
 
-# Avg Lot Size
-print(round(home_data['LotArea'].mean()))
+# Get Sale Price
+y = home_data.SalePrice
 
-# least home age
-print(pd.to_datetime('now').year - home_data['YearBuilt'].max())
+# Get predictive features
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+# Create a new Data Frame based on features
+X = home_data[features]
+
+# Create model based on features
+iowa_model = DecisionTreeRegressor(random_state=5)
+
+# Fit the model
+# X = Prediction features
+# y = prediction result [SalePrice]
+iowa_model.fit(X, y)
+
+# Predict X itself
+predictions = iowa_model.predict(X)
+
+# Get absolute errors between datasets and predictions
+# error = actual − predicted
+error = mean_absolute_error(y, predictions)
