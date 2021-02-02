270aef81d4f3e0a532e19b14b98feaedacf9da2d
Saddam Azy <myxzlpltk@gmail.com>
2021-02-02 20:06:47 +0700

    random forest regressor

--- Intro to Machine Learning/main.py
+++ Intro to Machine Learning/main.py
@@ -22,34 +22,8 @@ X = home_data[features]
 # Split data
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
-def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    # Create a model
-    model = DecisionTreeRegressor(random_state=1, max_leaf_nodes=max_leaf_nodes)
-    # Fit the data
-    model.fit(train_X, train_y)
-    # Predict
-    predictions = model.predict(val_X)
-    # Get MAE
-    mae = mean_absolute_error(predictions, val_y)
-    # Return mae
-    return (mae)
-
-# Get candidate
-candidate_max_leaf_nodes = range(5, 505, 5)
-# Testing candidate
-maes = []
-for max_leaf_node in candidate_max_leaf_nodes:
-    maes.append((max_leaf_node, get_mae(max_leaf_node, train_X, val_X, train_y, val_y)))
-
-best_max_leaf_node = min(maes, key = lambda sub: sub[1])[0]
-# best_max_leaf_node = 70
-
-# Plotting
-plt.plot(list(map(lambda x : x[0], maes)), list(map(lambda x : x[1], maes)))
-plt.title('Decision Tree Regression')
-plt.xlabel('Max Leaf Nodes')
-plt.ylabel('Mean Absolute Error')
-plt.show()
+# best_max_leaf_node
+best_max_leaf_node = 70
 
 # Create a final model
 final_model = DecisionTreeRegressor(max_leaf_nodes=best_max_leaf_node)
@@ -60,3 +34,14 @@ final_model.fit(train_X, train_y)
 # Predict
 predictions = final_model.predict(val_X)
 mae = mean_absolute_error(predictions, val_y)
+print(mae)
+
+# Create Random Forest Model
+rf_model = RandomForestRegressor()
+
+# Fitting model
+rf_model.fit(train_X, train_y)
+
+# Get absolute error
+rf_val_mae = mean_absolute_error(rf_model.predict(val_X), val_y)
+print(rf_val_mae)
